0338d0253d28958212c239bb0e695724c2208d00
rkt1234 <tiwari.ravikant2001@gmail.com>
2024-08-07 17:18:29 +0530

    "Updated HomeScreen with carousel slider, cached network image, and product tile; added dependencies to pubspec.yaml and pubspec.lock; modified GeneratedPluginRegistrant.swift"

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:ecommerce/widgets/product_tile.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,11 +10,18 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool isselected = false;
+  List<String> cat = ["All", "Clothes", "Mobile", "Laptop"];
+  String selectedCategory = "All";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text("Shopnow"),
+        backgroundColor: Colors.red,
+      ),
       bottomNavigationBar: BottomNavigationBar(
+        elevation: 0,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
@@ -20,6 +29,106 @@ class _HomeScreenState extends State<HomeScreen> {
               icon: Icon(Icons.shopping_bag), label: "Cart"),
         ],
       ),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+              const SizedBox(height: 20.0),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Text(
+                  'Featured Products',
+                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(height: 10.0),
+              CarouselSlider(
+                options: CarouselOptions(
+                  height: 200.0,
+                  autoPlay: true,
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enableInfiniteScroll: true,
+                  autoPlayAnimationDuration: const Duration(milliseconds: 200),
+                  viewportFraction: 1,
+                ),
+                items: [
+                  Image.network(
+      height: 200,
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvS7Bc0GHMNYqESMoWRv--SG95YQgpRSpicQ&s",
+      fit: BoxFit.cover,
+      width: double.infinity,
+                  ),
+                  Image.network(
+      height: 200,
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvS7Bc0GHMNYqESMoWRv--SG95YQgpRSpicQ&s",
+      fit: BoxFit.cover,
+      width: double.infinity,
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 20.0),
+                  const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Text(
+        'Categories',
+        style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+      ),
+                  ),
+                  const SizedBox(height: 10.0),
+                  Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      height: 50,
+      child: ListView.builder(
+        itemCount: cat.length,
+        // shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: ChoiceChip(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              label: Text(cat[index]),
+              selected: selectedCategory == cat[index],
+              onSelected: (selected) {
+                setState(() {
+                  selectedCategory = cat[index];
+                });
+              },
+            ),
+          );
+        },
+      ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10,),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: const Text(
+                            'Products',
+                            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                          ),
+              ),
+          const SizedBox(height: 10,),
+               Flexible(
+                 child: Container(
+                  // decoration: BoxDecoration(border: Border.all(color: Colors.black)),r
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                   child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: 190,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                        child: const ProductTile(),
+                      );
+                    },
+                   ),
+                 ),
+               )
+            ],
+           
+            ),
     );
   }
 }
---
+++ lib/widgets/product_tile.dart
@@ -0,0 +1,101 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class ProductTile extends StatefulWidget {
+  const ProductTile({super.key});
+
+  @override
+  State<ProductTile> createState() => _ProductTileState();
+}
+
+class _ProductTileState extends State<ProductTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      decoration: BoxDecoration(
+        color: Colors.white, // Background color
+        borderRadius: BorderRadius.circular(10), // Rounded corners
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5), // Shadow color
+            spreadRadius: 3, // Spread radius
+            blurRadius: 3, // Blur radius
+            // offset: Offset(0, 3), // Shadow position
+          ),
+        ],
+        // border: Border.all(
+        //     color: Colors.greenAccent, width: 2), // Border color and width
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(5),
+            child: CachedNetworkImage(
+              height: 100,
+              width: 150,
+              fit: BoxFit.cover,
+              imageUrl:
+                  "https://w0.peakpx.com/wallpaper/908/670/HD-wallpaper-dhoni-sports-uniform-cricket-ms-dhoni-mahendra-singh-dhoni-thumbnail.jpg",
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          const Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "titlehfilrewhiluhreilguilrgviureivhiorehviorehvioeh",
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "descriptionefhuirefvhreihvioreiovheohvodhfvh dhoc",
+                  maxLines: 1,
+                  overflow: TextOverflow.fade,
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "categoryfbjrefknrekgnvkreeikelkrnbvlkresklvnlkrfonfdelv",
+                  maxLines: 1,
+                  overflow: TextOverflow.fade,
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "pricejbfreuilfheilhfoirehgo;reo;gvho;eirhvoi;erajv",
+                  maxLines: 1,
+                  overflow: TextOverflow.fade,
+                  style: TextStyle(
+                    // color: Colors.green,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+
+  }
+}
